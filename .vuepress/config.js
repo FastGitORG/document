9cc69a91d6b341dda1f2ecbb00e33de9699572cc
Kevin S <33132228+KevinZonda@users.noreply.github.com>
2020-09-07 11:53:40 +0800

    config: add it-it

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,10 @@ const locales = {
     selectText: 'Languages',
     label: 'English',
   },
+  '/it-it/': {
+    selectText: 'Lingua',
+    label: 'Italiano',
+  },
   '/zh-cn/': {
     selectText: '选择语言',
     label: '简体中文',
@@ -24,6 +28,9 @@ module.exports = {
   locales: {
     '/en-us/': {
       lang: 'en-US',
+    },
+    '/it-it/': {
+      lang: 'it-IT',
     },
     '/zh-cn/': {
       lang: 'zh-CN',
